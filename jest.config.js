783c94290f2811c39cb3abe8ffce9ff14ddd09ac
Clay Branch <clay.branch@gmail.com>
2019-10-23 10:27:10 -0500

    Ignore Test component in coverage report

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 const {jest: jestConfig} = require('kcd-scripts/config')
 module.exports = Object.assign(jestConfig, {
+  testPathIgnorePatterns: ['<rootDir>/src/CounterList.js'],
   coverageThreshold: {
     global: {
       branches: 70,
