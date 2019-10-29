d4720ede6bf7b87ebdb610213d05b248c34df71f
Clay Branch <clay.branch@gmail.com>
2019-10-29 12:21:50 -0500

    docs: rework doc structure

--- docSite/website/siteConfig.js
+++ docSite/website/siteConfig.js
@@ -7,7 +7,7 @@
 
 // See https://docusaurus.io/docs/site-config for all the possible
 // site configuration options.
-
+const path = require('path')
 // List of projects/orgs using your project for the users page.
 const users = [
   {
@@ -18,7 +18,7 @@ const users = [
     infoLink: 'https://www.facebook.com',
     pinned: true,
   },
-];
+]
 
 const siteConfig = {
   title: 'Test Site', // Title for your website.
@@ -54,8 +54,8 @@ const siteConfig = {
 
   /* Colors for website */
   colors: {
-    primaryColor: '#349b70',
-    secondaryColor: '#246c4e',
+    primaryColor: '#8d7da1',
+    secondaryColor: '#625770',
   },
 
   /* Custom fonts for website */
@@ -91,6 +91,7 @@ const siteConfig = {
   // Open Graph and Twitter card images.
   ogImage: 'img/undraw_online.svg',
   twitterImage: 'img/undraw_tweetstorm.svg',
+  customDocsPath: `${path.basename(__dirname)}/docs`,
 
   // For sites with a sizable amount of content, set collapsible to true.
   // Expand/collapse the links and subcategories under categories.
@@ -105,6 +106,6 @@ const siteConfig = {
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
   //   repoUrl: 'https://github.com/facebook/test-site',
-};
+}
 
-module.exports = siteConfig;
+module.exports = siteConfig
