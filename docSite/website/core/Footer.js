c8bbafff381cb7465c2e3e7ee3967f6adeb08074
Clay Branch <clay.branch@gmail.com>
2019-11-25 14:16:26 -0600

    docs: fix star url

--- docSite/website/core/Footer.js
+++ docSite/website/core/Footer.js
@@ -55,7 +55,7 @@ class Footer extends React.Component {
               className="github-button"
               href={this.props.config.repoUrl}
               data-icon="octicon-star"
-              data-count-href="/facebook/docusaurus/stargazers"
+              data-count-href="/cbranch101/react-wiring-library/stargazers"
               data-show-count="true"
               data-count-aria-label="# stargazers on GitHub"
               aria-label="Star this project on GitHub"
